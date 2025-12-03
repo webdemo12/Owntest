40de1b7b18fc8ac131cd64d3ed9b48d032e51775
webdemo12 <gaytripura@gmail.com>
2025-12-04 02:21:34 +0530

    Add files via upload

---
+++ server/db.js
@@ -0,0 +1,14 @@
+import { Pool, neonConfig } from '@neondatabase/serverless';
+import { drizzle } from 'drizzle-orm/neon-serverless';
+import ws from "ws";
+
+neonConfig.webSocketConstructor = ws;
+
+if (!process.env.DATABASE_URL) {
+  throw new Error(
+    "DATABASE_URL must be set. Did you forget to provision a database?",
+  );
+}
+
+export const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+export const db = drizzle({ client: pool });
